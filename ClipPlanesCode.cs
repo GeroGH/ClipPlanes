d4c94818c6274ad7e81bc662fa207d1032543767
Gergan Gospodinov <gospodinovgergan@gmail.com>
2025-03-07 18:20:21 +0000

    Add project files.

---
+++ ClipPlanesCode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model;
+using ModelObjectSelector = Tekla.Structures.Model.UI.ModelObjectSelector;
+
+namespace ClipPlanes
+{
+    internal static class ClipPlanesCode
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var Points = new List<Point>();
+
+            var model = new Model();
+
+            ModelObjectEnumerator.AutoFetch = true;
+            var mos = new ModelObjectSelector();
+            var moe = mos.GetSelectedObjects();
+
+            while (moe.MoveNext())
+            {
+                var part = moe.Current as Part;
+                if (part == null)
+                {
+                    continue;
+                }
+
+                Points.Add(part.GetSolid().MaximumPoint);
+                Points.Add(part.GetSolid().MinimumPoint);
+            }
+
+            model.CommitChanges();
+        }
+    }
+}
