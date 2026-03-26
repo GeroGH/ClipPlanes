1e967ad821e45416a90c19f2d264ae675b174aa2
Gergan Gospodinov <gospodinovgergan@gmail.com>
2026-03-26 17:24:48 +0000

    Clear Clip Planes Added

--- ClipPlanesCode.cs
+++ ClipPlanesCode.cs
@@ -1,105 +1,180 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Forms;
 using Tekla.Structures.Geometry3d;
 using Tekla.Structures.Model;
+using Tekla.Structures.Model.Operations;
 using Tekla.Structures.Model.UI;
 
 namespace ClipPlanes
 {
     internal static class ClipPlanesCode
     {
+        private static Point MaxPoint = new Point();
+        private static Point MinPoint = new Point();
+
+        [STAThread]
         static void Main()
         {
-            var mve = ViewHandler.GetVisibleViews();
-            while (mve.MoveNext())
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new OptionSelectionForm());
+        }
+
+        public static void Execute(UserOption option)
+        {
+            if (option != UserOption.ClearPlanes)
             {
-                var currentView = mve.Current;
-                var clipPlanes = currentView.GetClipPlanes();
-                foreach (ClipPlane clipPlane in clipPlanes)
+                if (!ExtractPoints(option))
                 {
-                    clipPlane.Delete();
+                    Operation.DisplayPrompt("No valid parts selected.");
+                    return;
                 }
             }
-            mve.Reset();
 
-            var points = new List<Point>();
-            var model = new Model();
+            var visibleViews = ViewHandler.GetVisibleViews();
 
-            ModelObjectEnumerator.AutoFetch = true;
-            var mos = new Tekla.Structures.Model.UI.ModelObjectSelector();
-            var moe = mos.GetSelectedObjects();
-
-            if (moe.GetSize() == 0)
+            while (visibleViews.MoveNext())
             {
-                return;
-            }
+                var view = visibleViews.Current;
 
-            while (moe.MoveNext())
-            {
-                var part = moe.Current as Part;
-                if (part == null)
-                {
-                    continue;
-                }
-
-                points.Add(part.GetSolid().MaximumPoint);
-                points.Add(part.GetSolid().MinimumPoint);
-            }
-
-            var maxZPoint = points.OrderByDescending(p => p.Z).First();
-            var minZPoint = points.OrderByDescending(p => p.Z).Last();
-
-            var offset = 887;
-            OffsetPointInXYZ(maxZPoint, offset);
-            OffsetPointInXYZ(minZPoint, -offset);
-
-            using (var dialog = new OptionSelectionForm())
-            {
-                dialog.ShowDialog();
-
-                switch (dialog.SelectedOption)
+                switch (option)
                 {
                     case UserOption.DirectionX:
-                        while (mve.MoveNext())
-                        {
-                            CreateClipPlane(mve, maxZPoint, new Vector(1, 0, 0));
-                            CreateClipPlane(mve, minZPoint, new Vector(-1, 0, 0));
-                        }
-                        break;
-
                     case UserOption.DirectionY:
-                        while (mve.MoveNext())
-                        {
-                            CreateClipPlane(mve, maxZPoint, new Vector(0, 1, 0));
-                            CreateClipPlane(mve, minZPoint, new Vector(0, -1, 0));
-                        }
+                    case UserOption.DirectionZ:
+                        CreateClipPlanes(view, option);
                         break;
 
-                    case UserOption.DirectionZ:
-                        while (mve.MoveNext())
-                        {
-                            CreateClipPlane(mve, maxZPoint, new Vector(0, 0, 1));
-                            CreateClipPlane(mve, minZPoint, new Vector(0, 0, -1));
-                        }
+                    case UserOption.ClearPlanes:
+                        DeleteClipPlanes(view);
                         break;
                 }
             }
+
+            new Model().CommitChanges();
+
+            Operation.DisplayPrompt("Operation completed.");
         }
 
-        private static void CreateClipPlane(ModelViewEnumerator mve, Point maxZPoint, Vector vector)
+        private static bool ExtractPoints(UserOption direction)
         {
-            var upPlane = new ClipPlane();
-            upPlane.View = mve.Current;
-            upPlane.UpVector = vector;
-            upPlane.Location = maxZPoint;
+            const double offset = 250;
+
+            var points = new List<Point>();
+            var selector = new Tekla.Structures.Model.UI.ModelObjectSelector();
+            var objects = selector.GetSelectedObjects();
+
+            if (objects.GetSize() == 0)
+                return false;
+
+            while (objects.MoveNext())
+            {
+                if (objects.Current is Part part)
+                {
+                    var solid = part.GetSolid();
+                    points.Add(solid.MaximumPoint);
+                    points.Add(solid.MinimumPoint);
+                }
+            }
+
+            if (points.Count == 0)
+                return false;
+
+            switch (direction)
+            {
+                case UserOption.DirectionX:
+                    MaxPoint = points.Aggregate((a, b) => a.X > b.X ? a : b);
+                    MinPoint = points.Aggregate((a, b) => a.X < b.X ? a : b);
+                    break;
+
+                case UserOption.DirectionY:
+                    MaxPoint = points.Aggregate((a, b) => a.Y > b.Y ? a : b);
+                    MinPoint = points.Aggregate((a, b) => a.Y < b.Y ? a : b);
+                    break;
+
+                case UserOption.DirectionZ:
+                    MaxPoint = points.Aggregate((a, b) => a.Z > b.Z ? a : b);
+                    MinPoint = points.Aggregate((a, b) => a.Z < b.Z ? a : b);
+                    break;
+            }
+
+            OffsetPoint(MaxPoint, offset, direction);
+            OffsetPoint(MinPoint, -offset, direction);
+
+            return true;
+        }
+
+        private static void CreateClipPlanes(Tekla.Structures.Model.UI.View view, UserOption direction)
+        {
+            Vector vectorUp;
+            Vector vectorDown;
+
+            switch (direction)
+            {
+                case UserOption.DirectionX:
+                    vectorUp = new Vector(1, 0, 0);
+                    vectorDown = new Vector(-1, 0, 0);
+                    break;
+
+                case UserOption.DirectionY:
+                    vectorUp = new Vector(0, 1, 0);
+                    vectorDown = new Vector(0, -1, 0);
+                    break;
+
+                case UserOption.DirectionZ:
+                    vectorUp = new Vector(0, 0, 1);
+                    vectorDown = new Vector(0, 0, -1);
+                    break;
+
+                default:
+                    return;
+            }
+
+            var upPlane = new ClipPlane
+            {
+                View = view,
+                UpVector = vectorUp,
+                Location = MaxPoint
+            };
             upPlane.Insert();
+
+            var downPlane = new ClipPlane
+            {
+                View = view,
+                UpVector = vectorDown,
+                Location = MinPoint
+            };
+            downPlane.Insert();
         }
 
-        private static void OffsetPointInXYZ(Point point, double offset)
+        private static void DeleteClipPlanes(Tekla.Structures.Model.UI.View view)
         {
-            point.X += offset;
-            point.Y += offset;
-            point.Z += offset;
+            var clipPlanes = view.GetClipPlanes();
+
+            foreach (ClipPlane cp in clipPlanes)
+            {
+                cp.Delete();
+            }
+        }
+
+        private static void OffsetPoint(Point point, double offset, UserOption direction)
+        {
+            switch (direction)
+            {
+                case UserOption.DirectionX:
+                    point.X += offset;
+                    break;
+
+                case UserOption.DirectionY:
+                    point.Y += offset;
+                    break;
+
+                case UserOption.DirectionZ:
+                    point.Z += offset;
+                    break;
+            }
         }
     }
 }
--- OptionSelectionForm.cs
+++ OptionSelectionForm.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using ClipPlanes;
 public enum UserOption
 {
     DirectionX,
     DirectionY,
-    DirectionZ
+    DirectionZ,
+    ClearPlanes,
+    KeepPlanesAfter
 }
 public partial class OptionSelectionForm : Form
 {
     private Button buttonY;
     private Button buttonZ;
+    private TextBox textBox1;
+    private ContextMenuStrip contextMenuStrip1;
+    private System.ComponentModel.IContainer components;
+    private GroupBox groupBox1;
+    private GroupBox groupBox2;
+    private TextBox textBox2;
+    private Button buttonClearPlanes;
+    private GroupBox groupBox3;
+    private GroupBox groupBox4;
     private Button buttonX;
 
     public UserOption SelectedOption { get; private set; }
@@ -22,36 +34,51 @@ public partial class OptionSelectionForm : Form
     }
     private void ButtonX_Click(object sender, EventArgs e)
     {
-        this.SelectedOption = UserOption.DirectionX;
-        this.Close();
+        ClipPlanesCode.Execute(UserOption.DirectionX);
     }
 
     private void ButtonY_Click(object sender, EventArgs e)
     {
-        this.SelectedOption = UserOption.DirectionY;
-        this.Close();
+        ClipPlanesCode.Execute(UserOption.DirectionY);
     }
 
     private void ButtonZ_Click(object sender, EventArgs e)
     {
-        this.SelectedOption = UserOption.DirectionZ;
-        this.Close();
+        ClipPlanesCode.Execute(UserOption.DirectionZ);
     }
 
+    private void ButtonClearPlanes_Click(object sender, EventArgs e)
+    {
+        ClipPlanesCode.Execute(UserOption.ClearPlanes);
+    }
     private void InitializeComponent()
     {
+        this.TopMost = true;
+        this.components = new System.ComponentModel.Container();
         this.buttonX = new System.Windows.Forms.Button();
         this.buttonY = new System.Windows.Forms.Button();
         this.buttonZ = new System.Windows.Forms.Button();
+        this.textBox1 = new System.Windows.Forms.TextBox();
+        this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+        this.groupBox1 = new System.Windows.Forms.GroupBox();
+        this.groupBox2 = new System.Windows.Forms.GroupBox();
+        this.textBox2 = new System.Windows.Forms.TextBox();
+        this.buttonClearPlanes = new System.Windows.Forms.Button();
+        this.groupBox3 = new System.Windows.Forms.GroupBox();
+        this.groupBox4 = new System.Windows.Forms.GroupBox();
+        this.groupBox1.SuspendLayout();
+        this.groupBox2.SuspendLayout();
+        this.groupBox3.SuspendLayout();
+        this.groupBox4.SuspendLayout();
         this.SuspendLayout();
         // 
         // buttonX
         // 
         this.buttonX.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
         this.buttonX.ForeColor = System.Drawing.Color.Red;
-        this.buttonX.Location = new System.Drawing.Point(12, 12);
+        this.buttonX.Location = new System.Drawing.Point(6, 19);
         this.buttonX.Name = "buttonX";
-        this.buttonX.Size = new System.Drawing.Size(115, 41);
+        this.buttonX.Size = new System.Drawing.Size(146, 47);
         this.buttonX.TabIndex = 0;
         this.buttonX.Text = "X";
         this.buttonX.UseVisualStyleBackColor = true;
@@ -61,9 +88,9 @@ public partial class OptionSelectionForm : Form
         // 
         this.buttonY.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
         this.buttonY.ForeColor = System.Drawing.Color.Green;
-        this.buttonY.Location = new System.Drawing.Point(12, 65);
+        this.buttonY.Location = new System.Drawing.Point(6, 72);
         this.buttonY.Name = "buttonY";
-        this.buttonY.Size = new System.Drawing.Size(115, 41);
+        this.buttonY.Size = new System.Drawing.Size(146, 47);
         this.buttonY.TabIndex = 1;
         this.buttonY.Text = "Y";
         this.buttonY.UseVisualStyleBackColor = true;
@@ -73,24 +100,103 @@ public partial class OptionSelectionForm : Form
         // 
         this.buttonZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
         this.buttonZ.ForeColor = System.Drawing.Color.DodgerBlue;
-        this.buttonZ.Location = new System.Drawing.Point(12, 118);
+        this.buttonZ.Location = new System.Drawing.Point(6, 125);
         this.buttonZ.Name = "buttonZ";
-        this.buttonZ.Size = new System.Drawing.Size(115, 41);
+        this.buttonZ.Size = new System.Drawing.Size(146, 47);
         this.buttonZ.TabIndex = 2;
         this.buttonZ.Text = "Z";
         this.buttonZ.UseVisualStyleBackColor = true;
         this.buttonZ.Click += new System.EventHandler(this.ButtonZ_Click);
         // 
+        // textBox1
+        // 
+        this.textBox1.Location = new System.Drawing.Point(6, 19);
+        this.textBox1.Name = "textBox1";
+        this.textBox1.Size = new System.Drawing.Size(151, 20);
+        this.textBox1.TabIndex = 3;
+        // 
+        // contextMenuStrip1
+        // 
+        this.contextMenuStrip1.Name = "contextMenuStrip1";
+        this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+        // 
+        // groupBox1
+        // 
+        this.groupBox1.Controls.Add(this.textBox1);
+        this.groupBox1.Location = new System.Drawing.Point(179, 16);
+        this.groupBox1.Name = "groupBox1";
+        this.groupBox1.Size = new System.Drawing.Size(163, 47);
+        this.groupBox1.TabIndex = 6;
+        this.groupBox1.TabStop = false;
+        this.groupBox1.Text = "Up distance:";
+        // 
+        // groupBox2
+        // 
+        this.groupBox2.Controls.Add(this.textBox2);
+        this.groupBox2.Location = new System.Drawing.Point(179, 69);
+        this.groupBox2.Name = "groupBox2";
+        this.groupBox2.Size = new System.Drawing.Size(163, 47);
+        this.groupBox2.TabIndex = 7;
+        this.groupBox2.TabStop = false;
+        this.groupBox2.Text = "Down Distance:";
+        // 
+        // textBox2
+        // 
+        this.textBox2.Location = new System.Drawing.Point(6, 19);
+        this.textBox2.Name = "textBox2";
+        this.textBox2.Size = new System.Drawing.Size(146, 20);
+        this.textBox2.TabIndex = 3;
+        // 
+        // buttonClearPlanes
+        // 
+        this.buttonClearPlanes.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        this.buttonClearPlanes.ForeColor = System.Drawing.Color.Black;
+        this.buttonClearPlanes.Location = new System.Drawing.Point(6, 19);
+        this.buttonClearPlanes.Name = "buttonClearPlanes";
+        this.buttonClearPlanes.Size = new System.Drawing.Size(146, 47);
+        this.buttonClearPlanes.TabIndex = 8;
+        this.buttonClearPlanes.Text = "Clear Planes";
+        this.buttonClearPlanes.UseVisualStyleBackColor = true;
+        this.buttonClearPlanes.Click += new System.EventHandler(this.ButtonClearPlanes_Click);
+        // 
+        // groupBox3
+        // 
+        this.groupBox3.Controls.Add(this.buttonX);
+        this.groupBox3.Controls.Add(this.buttonY);
+        this.groupBox3.Controls.Add(this.buttonZ);
+        this.groupBox3.Location = new System.Drawing.Point(12, 16);
+        this.groupBox3.Name = "groupBox3";
+        this.groupBox3.Size = new System.Drawing.Size(161, 184);
+        this.groupBox3.TabIndex = 9;
+        this.groupBox3.TabStop = false;
+        this.groupBox3.Text = "Clip Plane Direction:";
+        // 
+        // groupBox4
+        // 
+        this.groupBox4.Controls.Add(this.buttonClearPlanes);
+        this.groupBox4.Location = new System.Drawing.Point(179, 122);
+        this.groupBox4.Name = "groupBox4";
+        this.groupBox4.Size = new System.Drawing.Size(163, 78);
+        this.groupBox4.TabIndex = 10;
+        this.groupBox4.TabStop = false;
+        this.groupBox4.Text = "Reset:";
+        // 
         // OptionSelectionForm
         // 
-        this.ClientSize = new System.Drawing.Size(140, 167);
-        this.Controls.Add(this.buttonZ);
-        this.Controls.Add(this.buttonY);
-        this.Controls.Add(this.buttonX);
+        this.ClientSize = new System.Drawing.Size(349, 204);
+        this.Controls.Add(this.groupBox4);
+        this.Controls.Add(this.groupBox3);
+        this.Controls.Add(this.groupBox2);
+        this.Controls.Add(this.groupBox1);
         this.Location = new System.Drawing.Point(4128, 84);
         this.Name = "OptionSelectionForm";
         this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+        this.groupBox1.ResumeLayout(false);
+        this.groupBox1.PerformLayout();
+        this.groupBox2.ResumeLayout(false);
+        this.groupBox2.PerformLayout();
+        this.groupBox3.ResumeLayout(false);
+        this.groupBox4.ResumeLayout(false);
         this.ResumeLayout(false);
-
     }
 }
